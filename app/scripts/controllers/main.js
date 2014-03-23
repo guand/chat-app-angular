a809c985d8f408121a7b7aa6899f9ac3763150bb
Danny Guan <guan3d@gmail.com>
2014-03-23 16:34:10 +0400

    added the chat-app-angular stuff

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('chatAppAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
